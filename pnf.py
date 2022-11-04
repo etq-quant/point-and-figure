c995761fb178c94b067f5e4e4527b9c4b46341dc
ngkeanchewet <ngkeanchewet@gmail.com>
2022-11-04 11:47:25 +0800

    clena up the code

--- pnf.py
+++ pnf.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import plotly.graph_objects as go
+
+
 def rounding(val):
     if val > 0:
         return math.ceil(val)
@@ -8,12 +10,20 @@ def rounding(val):
         return math.floor(val)
     else:
         return 0
+
+
 def round_up(val, box):
     return round(math.ceil(val / box) * box, 4)
+
+
 def round_down(val, box):
     return round(math.floor(val / box) * box, 4)
+
+
 def get_sign(val):
     return val / abs(val)
+
+
 def run_pnf_plotly(tdf, BOX_SIZE, REVERSAL=3, DAY=300):
     tdf = tdf.sort_values("date").reset_index(drop=True).copy()
     company_name = tdf["name"][0]
@@ -45,11 +55,7 @@ def run_pnf_plotly(tdf, BOX_SIZE, REVERSAL=3, DAY=300):
                 sign = 1
                 max_h = h
     BOX = BOX_SIZE
-    START = (
-        round_down(data[0][0], BOX_SIZE)
-        if data[0][0] < data[0][1]
-        else round_up(data[0][1], BOX_SIZE)
-    )  # tdf["high"][0] // BOX_SIZE * BOX_SIZE
+
     changes = [
         c
         if a == b
@@ -69,25 +75,15 @@ def run_pnf_plotly(tdf, BOX_SIZE, REVERSAL=3, DAY=300):
     color = {-1: "#FF6347", 1: "#89C35C"}
     fill_color = {-1: "white", 1: "#89C35C"}
     line_width = {-1: 2, 1: 0}
-    chgStart = START
-    prev_y = None
+
     for ichg, d in enumerate(data):
         direction = d[-1]
         start_y = round_up(min(d[:2]), BOX_SIZE)
         end_y = round_up(max(d[:2]), BOX_SIZE) + BOX_SIZE / 100
-        # if ichg > 0 and direction == 1:
-        #     # previous direction is -1 (down), hence start_y = previous bar 2nd smallest point
-        #     #start_y = prev_y
-        #     start_y = round_up(min(d[:2]), BOX_SIZE)
-        # if ichg > 0 and direction == -1:
-        #     # previous direction is 1 (up), hence start_y = previous bar 2nd largest point
-        #     end_y = prev_y + BOX_SIZE / 100
+
         y = np.arange(start_y, end_y, BOX_SIZE)
         x = [ichg + 1] * len(y)
-        # if direction == 1:
-        #     prev_y = y[-2]
-        # elif direction == -1:
-        #     prev_y = y[1]
+
         fig.add_trace(
             go.Scatter(
                 x=x,
